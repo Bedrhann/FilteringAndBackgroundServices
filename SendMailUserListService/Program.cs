973cec9cf3b879c8a1b583b5540571bf3971843a
Bedirhan Şık <Bedirhan_98@outlook.com>
2022-06-27 13:44:44 +0300

    Update Program.cs

--- SendMailUserListService/Program.cs
+++ SendMailUserListService/Program.cs
@@ -1,26 +1,15 @@
 
-using Hafta._5.Application.Interfaces.Services;
-using Hafta._5.Infrastructure.Services.UserExportService;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using SendMailUserListService;
 
-var host = CreateDefaultBuilder().Build();
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(configure =>
+    {
+        configure.ServiceName = "SendMailUserListService";
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
 
-// Invoke Worker
-using IServiceScope serviceScope = host.Services.CreateScope();
-IServiceProvider provider = serviceScope.ServiceProvider;
-var workerInstance = provider.GetRequiredService<Worker>();
-//workerInstance.DoWork();
-
-host.Run();
-static IHostBuilder CreateDefaultBuilder()
-{
-    return Host.CreateDefaultBuilder()
-        .ConfigureServices(services =>
-        {
-            services.AddSingleton<Worker>();
-            services.AddScoped<IUserExportExcel, UserExportExcel>();
-        });
-}
+await host.RunAsync();
